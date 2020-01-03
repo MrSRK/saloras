b7e989ba99b8a9406f738d71ef813c5890bc07cc
MrSRK <rubes6@hotmail.com>
2020-01-03 14:04:48 +0200

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Default Under Construction Page For saloras.gr
---
+++ app.js
@@ -0,0 +1,28 @@
+const express=require('express')
+const pug=require('pug')
+
+const path=require('path')
+
+const app=new express()
+
+app.set('views', path.join(__dirname, 'views'))
+	app.set('view engine','pug')
+
+app.use('/',express.static(path.join(__dirname,'/public')))
+app.use('/js/lib',express.static(path.join(__dirname,'/node_modules/jquery/dist')))
+app.use('/js/lib',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/js')))
+app.use('/webfonts',express.static(path.join(__dirname,'/node_modules/@fortawesome/fontawesome-free/webfonts')))
+app.use('/favicon.ico',express.static(path.join(__dirname,'/public/images/favicon.ico')))
+
+app.get('/',(req,res)=>
+{
+    return res.status(200).render('home',{
+    })
+})
+app.all('*',(req,res)=>
+{
+    //redirect to hame page
+    res.status(404).send('asdasd')
+})
+
+app.listen(80)
